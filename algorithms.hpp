6586b3acf1125b121a75d949f17238c68b774d45
AnthonyUdovichenko <antonyud2000@yandex.ru>
2021-05-18 16:30:06 +0300

    Небольшие улучшения кода

--- algorithms.hpp
+++ algorithms.hpp
@@ -7,27 +7,27 @@
 
 class Algorithms {
 public:
-    static int FirstFit(const std::vector<std::vector<long double>>& servers, const std::vector<long double>& resources) {
+    static int FirstFit(const std::vector<std::vector<long double>>& servers, const std::vector<long double>& vm) {
         for (int i = 0; i < servers.size(); ++i) {
-            if (Fits(servers[i], resources)) {
+            if (Fits(servers[i], vm)) {
                 return i;
             }
         }
         return -1;
     }
 
-    static int BestFit(const std::string& heuristic, const std::vector<std::vector<long double>>& servers, const std::vector<long double>& resources) {
+    static int BestFit(const std::string& heuristic, const std::vector<std::vector<long double>>& servers, const std::vector<long double>& vm) {
         long double best_value = 0;
         int best_ind = -1;
         for (int i = 0; i < servers.size(); ++i) {
-            if (Fits(servers[i], resources)) {
+            if (Fits(servers[i], vm)) {
                 long double value;
                 if (heuristic == "sum") {
                     value = sum(servers[i]);
                 } else if (heuristic == "prod") {
                     value = prod(servers[i]);
                 } else {
-                    std::cout << "For BestFit" << std::endl;
+                    std::cout << "In BestFit" << std::endl;
                     std::cout << "No such heuristic: " << heuristic << std::endl;
                     exit(0);
                 }
